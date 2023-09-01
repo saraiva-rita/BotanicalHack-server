4e24e89ccf5e8890a262231664852eec1569f781
Filipa Flora <filipa.flora@gmail.com>
2023-09-01 17:31:41 +0100

    added profile route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,35 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // ℹ️ Handles password encryption
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 // ℹ️ Handles password encryption
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 // Require the User model in order to interact with the database
-const User = require('../models/User.model');
+const User = require("../models/User.model");
 
 // Require necessary (isAuthenticated) middleware in order to control access to specific routes
-const { isAuthenticated } = require('../middleware/jwt.middleware.js');
+const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 
 // How many rounds should bcrypt run the salt (default - 10 rounds)
 const saltRounds = 10;
 
 // POST /auth/signup  - Creates a new user in the database
-router.post('/signup', (req, res, next) => {
+router.post("/signup", (req, res, next) => {
   const { email, password, name } = req.body;
 
   // Check if email or password or name are provided as empty strings
-  if (email === '' || password === '' || name === '') {
-    res.status(400).json({ message: 'Provide email, password and name' });
+  if (email === "" || password === "" || name === "") {
+    res.status(400).json({ message: "Provide email, password and name" });
     return;
   }
 
   // This regular expression check that the email is of a valid format
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
   if (!emailRegex.test(email)) {
-    res.status(400).json({ message: 'Provide a valid email address.' });
+    res.status(400).json({ message: "Provide a valid email address." });
     return;
   }
 
@@ -38,7 +38,7 @@ router.post('/signup', (req, res, next) => {
   if (!passwordRegex.test(password)) {
     res.status(400).json({
       message:
-        'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.',
+        "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
     });
     return;
   }
@@ -48,7 +48,7 @@ router.post('/signup', (req, res, next) => {
     .then((foundUser) => {
       // If the user with the same email already exists, send an error response
       if (foundUser) {
-        res.status(400).json({ message: 'User already exists.' });
+        res.status(400).json({ message: "User already exists." });
         return;
       }
 
@@ -75,12 +75,12 @@ router.post('/signup', (req, res, next) => {
 });
 
 // POST  /auth/login - Verifies email and password and returns a JWT
-router.post('/login', (req, res, next) => {
+router.post("/login", (req, res, next) => {
   const { email, password } = req.body;
 
   // Check if email or password are provided as empty string
-  if (email === '' || password === '') {
-    res.status(400).json({ message: 'Provide email and password.' });
+  if (email === "" || password === "") {
+    res.status(400).json({ message: "Provide email and password." });
     return;
   }
 
@@ -89,7 +89,7 @@ router.post('/login', (req, res, next) => {
     .then((foundUser) => {
       if (!foundUser) {
         // If the user is not found, send an error response
-        res.status(401).json({ message: 'User not found.' });
+        res.status(401).json({ message: "User not found." });
         return;
       }
 
@@ -105,27 +105,68 @@ router.post('/login', (req, res, next) => {
 
         // Create a JSON Web Token and sign it
         const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
-          algorithm: 'HS256',
-          expiresIn: '6h',
+          algorithm: "HS256",
+          expiresIn: "6h",
         });
 
         // Send the token as the response
         res.status(200).json({ authToken: authToken });
       } else {
-        res.status(401).json({ message: 'Unable to authenticate the user' });
+        res.status(401).json({ message: "Unable to authenticate the user" });
       }
     })
     .catch((err) => next(err)); // In this case, we send error handling to the error handling middleware.
 });
 
 // GET  /auth/verify  -  Used to verify JWT stored on the client
-router.get('/verify', isAuthenticated, (req, res, next) => {
+router.get("/verify", isAuthenticated, (req, res, next) => {
   // If JWT token is valid the payload gets decoded by the
   // isAuthenticated middleware and is made available on `req.payload`
   console.log(`req.payload`, req.payload);
 
   // Send back the token payload object containing the user data
   res.status(200).json(req.payload);
+});
+
+// Function to extract email from JWT token
+function getEmailFromToken(token, secretKey) {
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    return decoded.email; // Assuming your JWT payload includes an 'email' field
+  } catch (error) {
+    // Handle token verification error here
+    throw new Error("Invalid token");
+  }
+}
+
+router.get("/profile", isAuthenticated, async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const email = getEmailFromToken(token, process.env.TOKEN_SECRET);
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+    
+    // Return user information
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error." });
+  }
+});
+
+router.put("/profile/editName", isAuthenticated, async (req, res) => {
+  try {
+    const { name } = req.body;
+    const currentUser = req.payload;
+    let updatedName = await User.findByIdAndUpdate(currentUser._id, { name });
+
+    res.json(updatedName);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 module.exports = router;
